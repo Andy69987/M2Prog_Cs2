618720e19220d7ff3a8e76a329366d0a2a396d74
andor <andor@MSI>
2024-11-22 10:22:45 +0100

    commit

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // De functie heet Main returntype: void
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program(); // Hier wordt de constructor Program() gebruikt returntype: Program
+            program.Run(); // De functie heet Run returntype: void
+        }
+
+        // Hier is me functie
+        internal void Run() // De functie heet Run returntype: void
+        {
+            Console.WriteLine("Dit is nu de start van mijn programma"); // Geeft een boodschap weer
         }
     }
 }
