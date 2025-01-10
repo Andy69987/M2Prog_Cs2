e5ebd812f0ad34acc06701ebed0e7f4407c76cff
andor <andor@MSI>
2025-01-10 10:42:12 +0100

    push

---
+++ RemoveInLoop/RemoveInLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoveInLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animals = new List<string>
+            {
+                "hond", "kat", "uil", "kraai", "hond", "papegaai", "hond"
+            };
+
+            for (int i = 0; i < animals.Count; i++)
+            {
+                if (animals[i] == "hond")
+                {
+                    animals.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            for (int i = 0; i < animals.Count; i++)
+            {
+                Console.WriteLine(animals[i]);
+            }
+        }
+    }
+}
