3ca57159c115ea51faff069d139da53bea170e5a
andor <andor@MSI>
2025-01-10 10:44:53 +0100

    push

---
+++ LinqOpdracht/LinqOpdracht/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>
+            {
+                "Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace"
+            };
+
+            var namesStartingWithA = names.Where(name => name.StartsWith("A"));
+            var sortedNames = names.OrderBy(name => name);
+            var nameLengths = names.Select(name => name.Length);
+
+            Console.WriteLine("Names starting with A:");
+            foreach (var name in namesStartingWithA)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("\nSorted names:");
+            foreach (var name in sortedNames)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("\nName lengths:");
+            foreach (var length in nameLengths)
+            {
+                Console.WriteLine(length);
+            }
+        }
+    }
+}
