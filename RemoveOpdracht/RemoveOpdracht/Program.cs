55a75a8a453d54a3e7cef2dc86bb99814d57b3ed
andor <andor@MSI>
2025-01-10 10:30:24 +0100

    push

---
+++ RemoveOpdracht/RemoveOpdracht/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoveOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> characters = new List<string>
+            {
+                "Pac-Man",
+                "Samus",
+                "Crash Bandicoot",
+                "Spyro the Dragon",
+                "Donkey Kong",
+                "Mario",
+                "Luigi",
+                "Astro",
+                "Solid Snake",
+                "Master Chief",
+                "Kratos",
+                "Sonic the Hedgehog",
+                "Link"
+            };
+
+            characters.Remove("Crash Bandicoot");
+
+            for (int i = 0; i < characters.Count; i++)
+            {
+                Console.WriteLine(characters[i]);
+            }
+        }
+    }
+}
