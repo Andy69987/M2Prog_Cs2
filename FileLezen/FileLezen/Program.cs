6262e36c1f37f8d16e37e7a9fbeb7fbf038c4d25
andor <andor@MSI>
2025-01-31 09:18:24 +0100

    Push

---
+++ FileLezen/FileLezen/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string bestandspad = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "nos_artikel.txt");
+
+
+        try
+        {
+            string inhoud = File.ReadAllText(bestandspad);
+            Console.WriteLine("Inhoud van het NOS-artikel:");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine(inhoud);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Er is een fout opgetreden bij het lezen van het bestand:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
